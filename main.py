c7dea46475f68150e32abe89c412243f47e4945f
lalit-rg <rg.lalitkumar26@gmail.com>
2024-12-26 21:29:56 +0530

    data validation compeleted

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
-from networksecurity.components.model_trainer import ModelTrainer
+'''from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.entity.config_entity import ModelTrainerConfig
  
-
+'''
 import sys
 
 if __name__=='__main__':
@@ -27,7 +27,7 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
-        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        '''data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
         logging.info("data Transformation started")
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
@@ -42,6 +42,6 @@ if __name__=='__main__':
         logging.info("Model Training artifact created")
         
         
-        
+        '''
     except Exception as e:
            raise NetworkSecurityException(e,sys)
